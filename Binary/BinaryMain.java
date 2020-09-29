5ea308f62c1117fc5389ed86fe6cfda3393c7b28
arames34 <52871052+arames34@users.noreply.github.com>
2020-09-29 09:00:04 -0700

    "Added binary addition of two nums"

--- Binary/BinaryMain.java
+++ Binary/BinaryMain.java
@@ -3,8 +3,19 @@ package Binary;
 
 public class BinaryMain {
     public static boolean testSumOfTwoIntegers(){
-        return true;
+        int a =3;
+        int b= 6;
+        SumOfTwoIntegers s1 = new SumOfTwoIntegers(a, b);
+        if(s1.result == 9){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
+    
+
+
     
     public static void main(String[] args){
         int count =1;
--- Binary/SumOfTwoIntegers.java
+++ Binary/SumOfTwoIntegers.java
@@ -20,24 +20,57 @@ public class SumOfTwoIntegers {
         2) AND performs Carry
 
         How?
-        3 = 011
-        1 = 001
+        a =3 = 011
+        b =1 = 001
+        carry = a&b 001
+        a = a^b 010
+        b = carry << 1 == 010
+
         
         for every bit, the AND gate contains the carry
         and the XOR gate contains the sum
-
-        What we do is:
-        int addRes = 
-        
-        If u add both, you get the result
-
-
+       
     
     */
+    public void printBinary(int a){
+        int i =1;
+        StringBuilder b1 = new StringBuilder();
+        while(i !=0){
+            if((a&i) > 0){
+                b1.append(1);
+            }
+            else{
+                b1.append(0);
+            }
+            i = i<<1;
+        }
+        b1 = b1.reverse();
+        String res = b1.toString();
+        System.out.println(res); 
+    }
+
+
 
     int runSumOfTwoIntegers(){
-        
-        
-        return 0;
-    }
+        int count =0;
+
+        while (b != 0)  
+        {   System.out.println("Round"+ count);
+            printBinary(a);
+            printBinary(b);
+            // carry now contains common  
+            //set bits of x and y 
+            int carry = a & b;  // This will have the carry bits
+
+            // Sum of bits of x and y where at  
+            //least one of the bits is not set
+            a = a ^ b; 
+            
+            // Carry is shifted by one so that adding 
+            // it to x gives the required sum 
+            b = carry << 1; 
+            count++;
+        } 
+        return a; 
+   }
 }
