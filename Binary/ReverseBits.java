76633a7525a4dfcd989b8b083a0469b7e1180a6e
arames34 <52871052+arames34@users.noreply.github.com>
2020-09-30 13:04:15 -0700

    "Another way of solving this"

--- Binary/ReverseBits.java
+++ Binary/ReverseBits.java
@@ -25,4 +25,39 @@ public class ReverseBits {
         return s1.toString();
     }
 
+
+    // Take in integer and return integer, define the function as below
+    public int reverseBits(int n) {
+        StringBuilder s1 = new StringBuilder();
+        int count = 0; 
+        while(count !=32){
+            if((n&1) == 0){
+                s1.append(0);
+            }
+            else{
+                s1.append(1);
+            }
+            n= n>>1;
+            count++;
+        }
+        
+        s1.reverse();
+        String reversed = s1.toString();
+        
+        int result =0;
+        int multiplier =1;
+        for(char c: reversed.toCharArray()){
+            if(c =='1'){
+                result += multiplier;
+            }
+            multiplier = multiplier*2;
+            
+        } 
+        return result;
+        
+        
+    }
+
+
+
 }
