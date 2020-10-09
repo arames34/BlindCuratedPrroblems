492d416ef9092e78701232399398cbdbc9244d85
schaud28 <schaud28@asu.edu>
2020-10-08 20:54:29 -0700

    Commited climbing stairs removed spaces

---
+++ DynamicProgramming/CoinChange.java
@@ -0,0 +1,24 @@
+package DynamicProgramming;
+
+public class CoinChange {
+    
+    int[] input;
+    int amount;
+    boolean result;
+
+
+    CoinChange(int[] input, int amount){
+        this.input = input;
+        this.amount = amount;
+        this.result = runCoinChange();
+
+
+    }
+
+    boolean runCoinChange(){
+        
+        return true;
+    }
+
+
+}
--- DynamicProgramming/DynamicProgrammingMain.java
+++ DynamicProgramming/DynamicProgrammingMain.java
@@ -1,19 +1,11 @@
 package DynamicProgramming;
 
-
-
-
-
-
 public class DynamicProgrammingMain {
-
-
     public static boolean testClimbingStairs(){
             ClimbingStairs c = new ClimbingStairs(3);
             return (3 == c.result);
     }
     
-
     public static void main(String[] args){
         System.out.println("Running Climbing stairs test: Result: "+testClimbingStairs());
     }
