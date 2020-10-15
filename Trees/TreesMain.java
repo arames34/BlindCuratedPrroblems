16117385b5f4aa7eb3ecefbda3e697fcc059c562
schaud28 <schaud28@asu.edu>
2020-10-15 03:25:46 -0700

    "Serialization and deserialization Done right"

--- Trees/TreesMain.java
+++ Trees/TreesMain.java
@@ -119,3 +119,24 @@ public class TreesMain {
     
 
 }
+
+
+// Breaking down the idea!!   Beautiful problem if done in Java :P
+/*
+        1) Store the tree as a pre-order traversal string.  Root->Left->Right
+        2) During deserialization-> Remember to remove all the stop words and convert the string to a queue
+        3) After that, start building the tree. How?
+            1) Poll the front of the queue.
+                2) If null return null
+                3) If element is present
+                        a) create a new node initialized with this value.
+                        b) attach left subtree by calling the function recursively
+                        c) attach right subtree by calling the function recursively
+                            NOTE: Order same when u serialized.
+
+        To make sure you understand, printed out the tree! Everytime it generates random tree... do check out the code. 
+
+
+*/
+
+
