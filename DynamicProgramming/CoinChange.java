24a56bac67cc6f067435cdf4e3e17738b834dbf9
schaud28 <schaud28@asu.edu>
2020-10-14 22:29:47 -0700

    "Added new DP problems"

--- DynamicProgramming/CoinChange.java
+++ DynamicProgramming/CoinChange.java
@@ -4,21 +4,31 @@ public class CoinChange {
     
     int[] input;
     int amount;
-    boolean result;
-
+    int result;
 
     CoinChange(int[] input, int amount){
         this.input = input;
         this.amount = amount;
         this.result = runCoinChange();
-
-
     }
 
-    boolean runCoinChange(){
-        
-        return true;
+    int runCoinChange(){
+        int[] min = new int[amount+1];
+
+        for(int i=1; i<= amount; i++){
+            min[i] = Integer.MAX_VALUE;
+            for(int j =0; j<input.length; j++){
+                if(input[j] <= i&& min[i - input[j]] != Integer.MAX_VALUE){
+                      min[i] = Integer.min(min[i], 1+min[i-input[j]]);
+                }
+            }
+        }
+
+        if (min[amount] != Integer.MAX_VALUE){
+            return min[amount];
+        }
+        else{
+            return-1;
+        }
     }
-
-
 }
--- DynamicProgramming/DynamicProgrammingMain.java
+++ DynamicProgramming/DynamicProgrammingMain.java
@@ -1,12 +1,25 @@
 package DynamicProgramming;
 
+import javax.naming.spi.DirStateFactory.Result;
+
 public class DynamicProgrammingMain {
     public static boolean testClimbingStairs(){
             ClimbingStairs c = new ClimbingStairs(3);
             return (3 == c.result);
     }
-    
+
+    public static boolean testCoinChange(){
+        int[] input = {1,2,5};
+        CoinChange c = new CoinChange(input, 11);
+        return (3 == c.result);
+    }
+
+
+
+
     public static void main(String[] args){
         System.out.println("Running Climbing stairs test: Result: "+testClimbingStairs());
+        System.out.println("Running Coin Change test: Result: "+testCoinChange());
+        
     }
 }
